a037e898af56d49432c8901a35b2ed4090ae6bc9
DorianBestrzynski <80275992+DorianBestrzynski@users.noreply.github.com>
2022-06-08 14:59:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ def print_hi(name):
     '''Takes in name and print hello'''
     print(f'Hello {name}')
 
-
+def print_bye(name):
+    '''Takes in name and print bye'''
+    print(f'Goodbye {name}')
+    
 if __name__ == '__main__':
     print_hi('World')
+    print_bye('World')
